20aace81c7c85205c240c29798ab4a63aded9fe1
Colibryo <limagnin75@gmail.com>
2022-12-01 11:33:35 +0100

    Dans le fichier script, réduction du code pour générer les produits dans la page; Dans le fichier product.js, ajout de la fonctionnalité permettant d’ajouter les produits dans le panier

--- front/js/product.js
+++ front/js/product.js
@@ -1,17 +1,24 @@
+//recherche de l'url dans la page en cours - récupére l'id du produit avec la clé dans une variable
 let params = new URLSearchParams(document.location.search);
 let id = params.get("idProduct");
 
-
+/**requête pour obtenir les données json de tous les produits avec l'id récupéré
+ * la promesse retournée prend en paramètres le résultat pour déterminer son format
+ * retourne le résultat en cas de succès ou indique un message d'erreur*/
 fetch(`http://localhost:3000/api/products/${id}`)
+       //
        .then(response => {
-              if (response.ok) {
+              if (response) {
 
                      return response.json();
               }
               throw new Error('Erreur serveur');
 
        })
-
+       /* *promesse chaînée; si la réponse précédente est positive, la promesse prend en paramètre la réponse au format Json
+       * création des balises et attributs pour insérer l'image
+       * intégration du nom, de la description, et du prix du produit 
+       */
        .then(ProductJson => {
 
               document.querySelector('.item__img').innerHTML = "<img>"
@@ -21,10 +28,72 @@ fetch(`http://localhost:3000/api/products/${id}`)
               document.getElementById('description').innerText = ProductJson.description;
               document.getElementById('title').innerText = ProductJson.name;
               document.getElementById('price').innerText = ProductJson.price;
-              
-              for (eachColors of ProductJson.colors) {
-              document.querySelector('#colors').innerHTML += `<option value = ${eachColors}>${eachColors}</option>`;
-               
-       }
 
-       } )
+              //boucle pour ajouter automatiquement les options de couleurs des produits
+              for (eachColors of ProductJson.colors) {
+                     document.querySelector('#colors').innerHTML += `<option value = ${eachColors}>${eachColors}</option>`;
+              }
+              /*fonction pour ajouter les produits au localStorage
+              * prend en paramètre les données des produits
+              *intégre les produits avec leurs éléments dans le localStorage*/
+              function setBasket(tableBasket) {
+                     localStorage.setItem('basket', JSON.stringify(tableBasket));
+              }
+
+              //fonction qui crée le panier dans le localStorage s'il n'existe pas sinon elle récupère les données du panier qu'elle transorme au format json   
+              function getBasket() {
+                     let tableBasket = localStorage.getItem('basket');
+
+                     if (tableBasket === null) {
+                            return [];
+                     }
+
+                     else {
+                            return JSON.parse(tableBasket);
+                     }
+              }
+
+              /**fonction pour ajouter le produit sélectionné sur la page dans le panier : 
+              * si le produit existe déjà, la quantité du produit existant est ajoutée à celle du produit sélectionné dans la page, 
+              Après filtrage du panier pour ne pas récupérer le précédent produit, le produit regénéré avec sa nouvelle quantité 
+              est intégré dans le panier,  
+              *sinon la fonction ajoute le produit sélectionné dans le panier avec ses éléments : id, couleur et quantité  */
+              function addProduct() {
+
+                     let tableBasket = getBasket();
+                     let searchProduct = tableBasket.find(element => element.id === ProductJson._id & element.color === colors.value);
+
+                     if (searchProduct != undefined) {
+
+                            let newQuantity = parseInt(quantity.value) + parseInt(searchProduct.quantity)
+
+                            let newProduct = {
+                                   id: ProductJson._id,
+                                   color: colors.value,
+                                   quantity: parseInt(newQuantity)
+                            }
+
+                            let newTableBasket = tableBasket.filter(element => element.id !== ProductJson._id || element.color !== colors.value);
+                            newTableBasket.push(newProduct)
+                            setBasket(newTableBasket);
+                     }
+
+                     else {
+                            const productBasket = {
+                                   id: ProductJson._id,
+                                   color: colors.value,
+                                   quantity: parseInt(quantity.value)
+                            }
+
+                            tableBasket.push(productBasket);
+                            setBasket(tableBasket);
+                     }
+              }
+              //fonction qui, au click sur le bouton, ajoute le ou les produits au panier
+              const bouton = document.querySelector('#addToCart');
+              bouton.addEventListener('click', () => {
+                     addProduct();
+
+              })
+
+       })
--- front/js/script.js
+++ front/js/script.js
@@ -1,13 +1,5 @@
-/**class : objet cible copiant les propriétés de chaque produits
- *prend les données json renvoyées avec la requête fetch
- */
-class Products {
-       constructor(jsonProduct) {
-              jsonProduct && Object.assign(this, jsonProduct);
-       }
-};
-/**requête pour obtenir les données json de tous les produits 
- * prend en paramètres le résultat
+/**requête pour obtenir les données json de tous les produits avec l'id récupéré
+ * la promesse retournée prend en paramètres le résultat pour déterminer son format
  * retourne le résultat en cas de succès ou indique un message d'erreur*/
 fetch("http://localhost:3000/api/products")
        .then(response => {
@@ -22,12 +14,12 @@ fetch("http://localhost:3000/api/products")
         * récupère et génère en boucle les instances de chaque produits pour les afficher */
        .then(ProductsJson => {
               for (let jsonProduct of ProductsJson) {
-                     let product = new Products(jsonProduct)
-                     document.getElementById('items').innerHTML += `<a href="./product.html?idProduct=${product._id}">
+                     /*let product = new Products(jsonProduct)*/
+                     document.getElementById('items').innerHTML += `<a href="./product.html?idProduct=${jsonProduct._id}">
                                                         <article>
-                                                               <img src="${product.imageUrl}" alt="${product.altTxt}">
-                                                               <h3 class="productName">${product.name}</h3>
-                                                               <p class="productDescription">${product.description}</p>
+                                                               <img src="${jsonProduct.imageUrl}" alt="${jsonProduct.altTxt}">
+                                                               <h3 class="productName">${jsonProduct.name}</h3>
+                                                               <p class="productDescription">${jsonProduct.description}</p>
                                                         </article>
                                                  </a> `
               }
